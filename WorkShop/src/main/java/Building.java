cc9a92a10e66cb022957cd8b3c7d467575bb89a9
Rasmus Wittrock <rasmus.wittrock@gmail.com>
2021-11-17 10:08:55 +0100

    en smule funktionalitet, skulle ikke have været lavet endnu, my bad

--- WorkShop/src/main/java/Building.java
+++ WorkShop/src/main/java/Building.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Building {
+    ArrayList<Room> rooms = new ArrayList<>();
+    int ID = amountOfBuildings;
+    public static int amountOfBuildings;
+
+    public Building(int amountOfRooms){
+        for (int i = 0; i < i; i++) {
+            rooms.set(i, new Room(i));
+        }
+        amountOfBuildings +=1;
+    }
+    public ArrayList<Room> getRooms(){
+        return this.rooms;
+    }
+    public int getID(){
+        return this.ID;
+    }
 }
--- WorkShop/src/main/java/Room.java
+++ WorkShop/src/main/java/Room.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+
 public class Room {
+    int id;
+    ArrayList<Sensor> sensors = new ArrayList<>();
+
+    public Room(int id){
+        this.id = id;
+    }
 }
