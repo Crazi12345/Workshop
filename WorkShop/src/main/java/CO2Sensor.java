c39dd1dafd8c44295b493e2f60b1d47950a9a53d
Storm551 <stormfrederik@gmail.com>
2021-11-17 11:33:21 +0100

    Sensors

--- WorkShop/src/main/java/CO2Sensor.java
+++ WorkShop/src/main/java/CO2Sensor.java
@@ -1,2 +1,15 @@
+import java.util.UUID;
+
 public class CO2Sensor extends Sensor{
+    private double c02;
+    private UUID id;
+
+    public CO2Sensor(){
+        super();
+    }
+    @Override
+    public double getValue(){
+        return this.c02;
+    }
+
 }
