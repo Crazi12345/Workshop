0bc7276833dad868cd60368d40a525723ffc709a
Rasmus Wittrock <rasmus.wittrock@gmail.com>
2021-11-17 11:14:58 +0100

    added actuator class content

--- WorkShop/src/main/java/Actuator.java
+++ WorkShop/src/main/java/Actuator.java
@@ -1,2 +1,23 @@
+import java.util.UUID;
+
 public class Actuator {
+    UUID ID = UUID.randomUUID();
+    String action;
+    int setPoint = 0;
+
+    public Actuator(String action){
+        this.action = action;
+    }
+
+    public UUID getID(){
+        return this.ID;
+    }
+
+    public int getSetPoint(){
+        return this.setPoint;
+    }
+
+    public void setSetPoint(int i){
+        this.setPoint = i;
+    }
 }
